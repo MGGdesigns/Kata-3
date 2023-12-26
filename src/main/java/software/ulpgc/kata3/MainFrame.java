12befe01096e118684e7b50dd73d514665fb627f
Mauro Gómez <m.gomeznitoni@gmail.com>
2023-12-26 19:02:51 +0000

    MainFrame and JFreeChartHistogramDisplay classes created

---
+++ src/main/java/software/ulpgc/kata3/MainFrame.java
@@ -0,0 +1,28 @@
+package software.ulpgc.kata3;
+
+import org.jfree.chart.JFreeChart;
+
+import javax.swing.;
+import java.awt.;
+
+public class MainFrame extends JFrame {
+    private HistogramDisplay histogramDisplay;
+
+    public MainFrame() throws HeadlessException {
+        this.setTitle("Histograma");
+        this.setSize(900, 900);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.add(createHistogramDisplay());
+    }
+
+    private Component createHistogramDisplay() {
+        JFreeChartHistogramDisplay display = new JFreeChartHistogramDisplay();
+        this.histogramDisplay = display;
+        return display;
+    }
+
+    public HistogramDisplay histogramDisplay() {
+        return histogramDisplay;
+    }
+}
